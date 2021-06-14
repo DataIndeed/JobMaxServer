baff9fe4309f36095c3b73411289c80b1b9f0f46
Xiliang Zeng <zengxiliang@gmail.com>
2021-06-13 20:08:34 -0400

    remove main

--- application.py
+++ application.py
@@ -4,6 +4,3 @@ application = Flask(__name__)
 @application.route('/')
 def hello_world():
     return 'this will be job max main page.'
-
-if __name__ == "__main__":
-        application.run(host='0.0.0.0', port=5000)
