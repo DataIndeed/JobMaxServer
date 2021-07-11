d61ade4dd07b3b221050e6ca95abcde0ed406f65
DataIndeed <zengxiliang@gmail.com>
2021-07-11 14:35:21 -0400

    testr

--- application.py
+++ application.py
@@ -1,12 +1,12 @@
-#import boto3
+import boto3
 from flask import Flask, render_template
 application = Flask(__name__)
 
 @application.route('/')
 def hello_world():
-    #s3 = boto3.resource('s3')
-    #my_bucket = s3.Bucket('jobmaxresults/')
-    #urllist = my_bucket.objects.all()
-    urllist = ['https://jobmaxresults.s3.amazonaws.com/wordcloud_AWS+developer_2020-11-21.jpg',
-               'https://jobmaxresults.s3.amazonaws.com/wordcloud_AWS+developer_2020-11-21.jpg']
+    s3 = boto3.resource('s3')
+    my_bucket = s3.Bucket('jobmaxresults/')
+    urllist = my_bucket.objects.all()
+    #urllist = ['https://jobmaxresults.s3.amazonaws.com/wordcloud_AWS+developer_2020-11-21.jpg',
+    #           'https://jobmaxresults.s3.amazonaws.com/wordcloud_AWS+developer_2020-11-21.jpg']
     return render_template('index.html', urllist=urllist)
