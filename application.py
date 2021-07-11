75fc14e35be2b083bcd52d4dc8c7c561d5052910
DataIndeed <zengxiliang@gmail.com>
2021-07-10 23:16:18 -0400

    testr

--- application.py
+++ application.py
@@ -1,6 +1,11 @@
+import boto3
 from flask import Flask, render_template
 application = Flask(__name__)
 
 @application.route('/')
 def hello_world():
+    s3 = boto3.resource('s3')
+    my_bucket = s3.Bucket('jobmaxresults/')
+    for my_bucket_object in my_bucket.objects.all():
+        print(my_bucket_object)
     return render_template('index.html')
