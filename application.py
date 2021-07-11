50d4cc395c35a83989d13c8f40164209daca08e1
DataIndeed <zengxiliang@gmail.com>
2021-07-11 16:42:14 -0400

    testr

--- application.py
+++ application.py
@@ -8,7 +8,7 @@ def hello_world():
     my_bucket = s3.Bucket('jobmaxresults')
     #obj_list = s3.list_objects_v2(Bucket=my_bucket)
     objlist = my_bucket.objects.all()
-    keylist = [obj.getKey() for obj in objlist]
+    keylist = [obj['key'] for obj in objlist]
     #keylist = my_bucket.objects.all().keys()
     #urllist = ['https://jobmaxresults.s3.amazonaws.com/' + key['Key'] for key in urllist]
     #urllist = ['https://jobmaxresults.s3.amazonaws.com/wordcloud_AWS+developer_2020-11-21.jpg',
