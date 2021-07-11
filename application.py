4cf1dbcdb6b1c1e74f403b509eb8ede94433c7ec
DataIndeed <zengxiliang@gmail.com>
2021-07-11 15:50:27 -0400

    testr

--- application.py
+++ application.py
@@ -6,7 +6,9 @@ application = Flask(__name__)
 def hello_world():
     s3 = boto3.resource('s3')
     my_bucket = s3.Bucket('jobmaxresults')
-    urllist = my_bucket.objects.all()
+    #urllist = my_bucket.objects.all()
+    keylist = my_bucket.objects.get_all_keys()
+    urllist = ['https://jobmaxresults.s3.amazonaws.com/' + key for key in keylist]
     #urllist = ['https://jobmaxresults.s3.amazonaws.com/wordcloud_AWS+developer_2020-11-21.jpg',
     #           'https://jobmaxresults.s3.amazonaws.com/wordcloud_AWS+developer_2020-11-21.jpg']
     return render_template('index.html', urllist=urllist)
