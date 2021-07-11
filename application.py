1bc9e49b18af7a3146125913549a04234510ae0f
DataIndeed <zengxiliang@gmail.com>
2021-07-11 14:06:16 -0400

    testr

--- application.py
+++ application.py
@@ -7,4 +7,6 @@ def hello_world():
     s3 = boto3.resource('s3')
     my_bucket = s3.Bucket('jobmaxresults/')
     urllist = my_bucket.objects.all()
+    urllist = ['https://jobmaxresults.s3.amazonaws.com/wordcloud_AWS+developer_2020-11-21.jpg',
+               'https://jobmaxresults.s3.amazonaws.com/wordcloud_AWS+developer_2020-11-21.jpg']
     return render_template('index.html', urllist=urllist)
