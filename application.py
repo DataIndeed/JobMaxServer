c4c333ca80c2282d3cbbc0d1bb3e3b4eb775951a
DataIndeed <zengxiliang@gmail.com>
2021-07-11 16:25:01 -0400

    testr

--- application.py
+++ application.py
@@ -7,10 +7,11 @@ def hello_world():
     s3 = boto3.resource('s3')
     my_bucket = s3.Bucket('jobmaxresults')
     #obj_list = s3.list_objects_v2(Bucket=my_bucket)
-    urllist = my_bucket.objects.all()
+    objlist = my_bucket.objects.all()
+    keylist = [obj.getKey() for obj in objlist]
     #keylist = my_bucket.objects.all().keys()
     #urllist = ['https://jobmaxresults.s3.amazonaws.com/' + key['Key'] for key in urllist]
     #urllist = ['https://jobmaxresults.s3.amazonaws.com/wordcloud_AWS+developer_2020-11-21.jpg',
     #           'https://jobmaxresults.s3.amazonaws.com/wordcloud_AWS+developer_2020-11-21.jpg']
-    return render_template('index.html', urllist=urllist)
+    return render_template('index.html', urllist=keylist)
 
