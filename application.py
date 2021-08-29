7a6eeab5392dec2d38a2fc1a14b4b220e822c58d
Xiliang Zeng <zengxiliang@gmail.com>
2021-08-29 18:51:34 -0400

    allow submit search request

--- application.py
+++ application.py
@@ -1,11 +1,13 @@
 import string
 
+import werkzeug.utils as utils
 import boto3
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for
+
 application = Flask(__name__)
 
 @application.route('/')
-def hello_world():
+def index():
     s3 = boto3.resource('s3')
     my_bucket = s3.Bucket('jobmaxresults')
     #obj_list = s3.list_objects_v2(Bucket=my_bucket)
@@ -18,7 +20,17 @@ def hello_world():
     #keylist = my_bucket.objects.all().keys()
     #urllist = ['https://jobmaxresults.s3.amazonaws.com/' + key['Key'] for key in urllist]
     #urllist = ['https://jobmaxresults.s3.amazonaws.com/wordcloud_AWS+developer_2020-11-21.jpg',
-    #           'https://jobmaxresults.s3.amazonaws.com/wordcloud_AWS+developer_2020-11-21.jpg']
+    #'https://jobmaxresults.s3.amazonaws.com/wordcloud_AWS+developer_2020-11-21.jpg']
     return render_template('index.html', urllist=urllist)
 
-#application.run()
+
+@application.route('/analyze', methods=['POST'])
+def analyze():
+    job_title = request.form['job_title']
+    sqs = boto3.resource('sqs')
+    queue = sqs.get_queue_by_name(QueueName='JOBMAXSEARCH')
+    response = queue.send_message(MessageBody=job_title)
+
+    return utils.redirect(url_for('index'))
+
+application.run()
